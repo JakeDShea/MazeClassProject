f83e3030415a53486a51700227464e52d11e6a89
Jacob Shea <jdshea@email.wm.edu>
2021-10-18 18:08:09 -0400

    Creates Pseudocode for Wizard.java and Javadocs for WallFollower.java

--- MazeByJakeShea/src/gui/Wizard.java
+++ MazeByJakeShea/src/gui/Wizard.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.nio.charset.StandardCharsets;
+
 import generation.Maze;
 
 /**
@@ -14,40 +16,102 @@ import generation.Maze;
 
 
 public class Wizard implements RobotDriver {
-
+	//Sets up instance variables for the Wizard, such as the Robot and maze.
+	
+	/**
+	 * Assigns a robot platform to the driver. 
+	 * The driver uses a robot to perform, this method provides it with this necessary information.
+	 * @param r robot to operate
+	 */
 	@Override
 	public void setRobot(Robot r) {
-		// TODO Auto-generated method stub
-
+		//Sets up the Wizard's instance variable using a Robot passed as a parameter.
 	}
 
+	/**
+	 * Provides the robot driver with the maze information.
+	 * Only some drivers such as the wizard rely on this information to find the exit.
+	 * @param maze represents the maze, must be non-null and a fully functional maze object.
+	 */
 	@Override
 	public void setMaze(Maze maze) {
-		// TODO Auto-generated method stub
-
+		//Sets up the Wizard's maze instance variable using a Maze passed as a parameter.
 	}
 
+	/**
+	 * Drives the robot towards the exit following
+	 * its solution strategy and given the exit exists and  
+	 * given the robot's energy supply lasts long enough. 
+	 * When the robot reached the exit position and its forward
+	 * direction points to the exit the search terminates and 
+	 * the method returns true.
+	 * If the robot failed due to lack of energy or crashed, the method
+	 * throws an exception.
+	 * If the method determines that it is not capable of finding the
+	 * exit it returns false, for instance, if it determines it runs
+	 * in a cycle and can't resolve this.
+	 * @return true if driver successfully reaches the exit, false otherwise
+	 * @throws Exception thrown if robot stopped due to some problem, e.g. lack of energy
+	 */
 	@Override
 	public boolean drive2Exit() throws Exception {
-		// TODO Auto-generated method stub
+		//Tries to solve maze
+		
+		//Throws an exception if Wizard's Robot stops by means like losing energy or crashing
+		
+		//Returns false is Wizard's Robot ever walks on a cell it has already walked upon.
+		
+		//Returns true otherwise, meaning it has completed the maze.
 		return false;
 	}
 
+	/**
+	 * Drives the robot one step towards the exit following
+	 * its solution strategy and given the exit exists and 
+	 * given the robot's energy supply lasts long enough.
+	 * It returns true if the driver successfully moved
+	 * the robot from its current location to an adjacent
+	 * location.
+	 * At the exit position, it rotates the robot 
+	 * such that if faces the exit in its forward direction
+	 * and returns false. 
+	 * If the robot failed due to lack of energy or crashed, the method
+	 * throws an exception. 
+	 * @return true if it moved the robot to an adjacent cell, false otherwise
+	 * @throws Exception thrown if robot stopped due to some problem, e.g. lack of energy
+	 */
 	@Override
 	public boolean drive1Step2Exit() throws Exception {
-		// TODO Auto-generated method stub
+		//If Robot is at the exit, changes the Robot's direction to face the exit and returns false.
+		
+		//Else, moves the Robot 1 step in whatever the best direction is.
+		
+		//Will throw an error if robot stops moving
 		return false;
 	}
 
+	/**
+	 * Returns the total energy consumption of the journey, i.e.,
+	 * the difference between the robot's initial energy level at
+	 * the starting position and its energy level at the exit position. 
+	 * This is used as a measure of efficiency for a robot driver.
+	 * @return the total energy consumption of the journey
+	 */
 	@Override
 	public float getEnergyConsumption() {
-		// TODO Auto-generated method stub
+		//Returns Robot's starting energy minus it's final energy
 		return 0;
 	}
 
+	/**
+	 * Returns the total length of the journey in number of cells traversed. 
+	 * Being at the initial position counts as 0. 
+	 * This is used as a measure of efficiency for a robot driver.
+	 * @return the total length of the journey in number of cells traversed
+	 */
 	@Override
 	public int getPathLength() {
-		// TODO Auto-generated method stub
+		//Returns Robot's odometer reading.
 		return 0;
 	}
 
